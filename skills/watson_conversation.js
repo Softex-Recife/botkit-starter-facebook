9922bbab97bda9167f0f4e82e1fe15491b8d2086
bernardo <bernardodemsj@gmail.com>
2019-02-15 17:12:08 -0300

    suporte para quick replies e correcao package.json

--- skills/watson_conversation.js
+++ skills/watson_conversation.js
@@ -1,15 +1,15 @@
 module.exports = function(controller, assistant, contextos) {
-    controller.on('message_received', function(bot, message) {		
+    controller.on(['direct_message', 'interactive_message_callback','message_received', 'facebook_postback'], function(bot, message) {		
 		resolveMessage(assistant, contextos, bot, message)
     });
 };
 
 function resolveMessage(assistant, contextos, bot, message){
-	key = message.user;
+	var key = message.user;
 	var contexto = null;
 	if (!contextos[key]){
 		assistant.createSession({
-			assistant_id: "65431f3c-bf2d-4121-85fb-a78e5cb46ce2"
+			assistant_id: process.env.assistant_id
 		}, (err, response)=>{
 			if (err) {
 				console.error(err);
@@ -25,19 +25,46 @@ function resolveMessage(assistant, contextos, bot, message){
 	}
 }
 
+function createQuickReplies(list_buttons, texto) {
+	console.log("replies", list_buttons);
+    var quick_replies = [];
+    list_buttons.forEach(function (item) {
+        quick_replies.push({
+            "content_type":"text",
+            "title":item,
+            "payload":item
+        })
+    });
+    return {
+        "text": ""+texto,
+        "quick_replies":quick_replies
+    };
+}
+
 function sendMessage(contexto, message, bot, assistant){
 	assistant.message({
-		assistant_id: '65431f3c-bf2d-4121-85fb-a78e5cb46ce2',
+		assistant_id: process.env.assistant_id,
 		session_id: contexto,
 		input: {
 			'message_type': 'text',
-			'text': message.text
+			'text': message.text,
+			'options': {
+				'return_context': true
+			}
 		}
 		}, function(err, response) {
 			if (err) {
 				console.log('error:', err);
-			}else{					
-				bot.reply(message, response.output.generic[0].text);
+			}else{
+				//console.log('response', JSON.stringify(response, null, 2));
+				var quick_replies =  response.context.skills['main skill'].user_defined.quick_replies.replies
+				var resp = response.output.generic[0].text
+				if (typeof quick_replies !== 'undefined' && quick_replies.length > 0) {
+					response = createQuickReplies(quick_replies, resp)
+					bot.reply(message, response)
+				} else{
+					bot.reply(message, resp);
+				}	
 			}
 		}
 	);
